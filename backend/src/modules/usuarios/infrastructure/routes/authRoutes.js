ce1318920e861a3cf25056294b25e8999242a61c
Hernán Pereira <pereirahernan@outlook.com>
2024-08-18 11:26:30 -0500

    feat(backend): :sparkles: Setup auth control

--- backend/src/modules/usuarios/infrastructure/routes/authRoutes.js
+++ backend/src/modules/usuarios/infrastructure/routes/authRoutes.js
@@ -3,10 +3,16 @@ const express = require('express');
 const authController = require('../controllers/AuthController')
 const validateRequest = require('../../../shared/infrastructure/middlewares/validateRequest')
 const { registerUsuarioSchema } = require('../validators/usuarioValidator')
+const passport = require('../../../shared/infrastructure/middlewares/passportJwtStrategy')
 
 const router = express.Router();
 
 router.post('/register', validateRequest(registerUsuarioSchema), authController.register)
 router.post('/login', authController.login);
+
+// Rutas protegidas
+router.get('/perfil', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.status(200).json({ usuario: req.user });
+});
 
 module.exports = router;
