6ca641ceba8f63d5adefbee9055a3cca6fe6b605
Hernán Pereira <pereirahernan@outlook.com>
2024-08-20 01:20:06 -0500

    refactor(frontend): :zap: Setup nextjs proxy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://backend:5000/api/:path*', // Redirige las solicitudes al backend
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
